43b614defd96abced8a227799d095ccd298bb355
SantiagoAM <s.acostam23@uniandes.edu.co>
2023-09-07 17:35:27 -0500

    Coreccion

--- Taller_1/parte_1/1_2.sh
+++ Taller_1/parte_1/1_2.sh
@@ -1,24 +1,22 @@
-# Adquirimos el archivo CSV con la informacion
+# Adquirimos el archivo CSV con la información
 csv="indices_refraccion.csv"
 
 # Creamos una carpeta para guardar los archivos yml
 output_folder="archivos_yml"
 mkdir -p "$output_folder"
 
-# Leemos la informacion del CSV
+# Leemos la información del CSV
 while IFS=',' read -r categoria fabricante material enlace; do
-    # Eliminar los retornos de carro de la URL
+    # Eliminar los retornos de carro de la URL y el material
     enlace=$(echo "$enlace" | tr -d '\r')
+    material=$(echo "$material" | tr -d '\r')
 
-    # Si la categoria no existe creamos una nueva carpeta
-
+    # Si la categoría no existe, creamos una nueva carpeta
     categoria_folder="$output_folder/$categoria"
     mkdir -p "$categoria_folder"
 
-    # Miramos el nombre del archivo yml
-    nombre=$(basename "$enlace")
-
-    # Descargamo el archivo yml y lo guardamos
+    # Descargamos el archivo yml y lo guardamos con el nombre del material
+    nombre="$material.yml"
     wget -O "$categoria_folder/$nombre" "$enlace"
 
 done < <(tail -n +2 "$csv")
